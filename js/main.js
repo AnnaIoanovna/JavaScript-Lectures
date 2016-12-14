ab4723826c8fbc29126db5c1fbe50e9a0ea96aa9
AnnaIoanovna <ann.palazhchenko@gmail.com>
2016-12-14 23:17:41 +0200

    Lecture2 operators

---
+++ js/main.js
@@ -0,0 +1,99 @@
+"use strict"; //строгий режим написания кода
+// alert("Hello world!");
+// console.log("Hello console");
+// var myCat = "Skif";
+// // debugger;
+// var MY_CONSTANT = "Svyatik";
+// var a = 10;
+// myCat = "Kostya";
+// // console.log(myCat, a);
+// var object = {
+// 	name: "Svyatik",
+// 	"age": 2
+// };
+// var array = ["friday", "sat", myCat];
+
+// var name = "age";
+
+// var newObject = object;
+
+// console.log(array);
+
+// console.log(array[0]);
+
+// console.log(object);
+
+// console.log(object[name]);
+
+// console.log(object.age);
+
+// object.age = 3;
+
+// object.toy = "mom";
+
+// console.log(object.age);
+
+// console.log(newObject);
+
+
+// array["newParam"] = "buhlo";
+
+// console.log(array);
+
+// array[11] = "new elem";
+
+// array.push("eda");
+
+// console.log(array);
+
+// var myFunk = function () {}
+
+// function  newFunction () {
+// 	alert(666);
+// }
+
+// //funktion is {};
+// //array is {};
+
+// newFunction();
+
+// newFunction.newProperty = "text";
+
+// console.log(newFunction.newProperty); 
+
+// function drinking() {
+// 	alert (drinking.drink);
+// 	return "I want to drink " + drinking.drink;
+// }
+// drinking.drink = "Buhlo!!!";
+
+// drinking();
+
+// console.log(drinking()); 
+
+// console.log(3 > 5);
+// console.log(3 > '5'); 
+// console.log(null == undefined);
+// console.log(0 == null);
+// console.log("5 == '5'", 5 == '5');
+// console.log("5 === '5'", 5 === '5');
+
+// console.log(2 == 2 && 3 == 3);
+
+// console.log(2 == 2 && 5 == 3);
+
+// var day = "Friday";
+
+// console.log(day == "Friday" && drinking());
+
+// console.log(day != "Friday" && drinking());
+
+// console.log(day != "Friday" || drinking());
+
+console.log(!false); 
+
+console.log(!"text");
+
+console.log("text");
+
+console.log(!!"text");
