976f20a54521d7a8b0c5c01c5d2fa31bfae977c9
AnnaIoanovna <ann.palazhchenko@gmail.com>
2016-12-18 16:12:38 +0200

    operators if, swich

--- js/main.js
+++ js/main.js
@@ -90,10 +90,101 @@
 
 // console.log(day != "Friday" || drinking());
 
-console.log(!false); 
+// console.log(!false); 
 
-console.log(!"text");
+// console.log(!"text");
 
-console.log("text");
+// console.log("text");
 
-console.log(!!"text");
+// console.log(!!"text");
+
+// !"", !0, [], {}, function () {}, true // true
+// "", 0, null, undefined, NaN, false // false
+
+//&& - логическое и - приоритет выше
+//|| - логическое или 
+// ! - логическое не
+
+console.log(1 && 2 || 5 && 6);
+
+console.log(1 && false || false && 6);
+
+console.log('!"sleep" =', !"sleep"); //false
+
+console.log('!0 = ', !0); // true
+
+console.log('!!null =', !!null); // false
+
+var x, y;
+x = 5, 6;
+y = (5,6);
+
+console.log(x);
+
+//инкремент,декремент
+//++ увеличить на единицу
+//-- уменьшить на единицу
+// Операторы инкремента и декремента МЕНЯЮТ исодную переменную
+var x = 10;
+
+x++; // постфикс показывает значение до прибавления
+
+console.log(x);
+
+++x; // префикс показывает значение после прибавления
+
+console.log(x);
+console.log(x++, ++x);
+console.log(++x, x++);
+
+// Особенности операторов сравнения 
+
+console.log("'x' > 'y' =", 'x' > 'y');
+
+console.log("'x' > 'Y' =", 'x' > 'Y');
+
+console.log("'test' > 'tes' =", 'test' > 'tes');
+
+console.log("'1' > '2' =", '1' > '2');
+
+console.log("'12' > '2' =", '12' > '2');
+
+console.log("+'12' > +'2' =", +'12' > +'2');
+
+// var x = NaN;
+// if (typeof x === "number" && x === x) {
+// 	console.log(x); // It's number and not a NaN
+// };
+
+// var day = "monday";
+// console.log( true == 1);
+ 
+//  if (day=="friday") {
+//  	console.log("Drink Buhlo");
+//  } 
+// else {
+// 	console.log("Drink tea");
+// }
+
+
+// if (day=="friday") {
+// 	console.log("Drink Buhlo"); 
+// }
+// else if (day=="saturday") {
+// 	console.log("Vseravno buhlo");
+// }
+// else {
+// 	console.log("Tea!!!");
+// }
+
+var day = "Friday";
+switch (day){
+	case "Friday":
+		alert("Ура, опять бухать!");
+		break;
+	case "Monday":
+		alert("Фуу!!! Опять бодун.");
+		break;
+	default:	
+		alert("Ну как повезет");	
+}
