ba798856514932ebc0aa6d8290787a1f7df6ca86
AnnaIoanovna <ann.palazhchenko@gmail.com>
2016-12-18 23:40:34 +0200

    Masivi

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var i = 0;
+while (i < 5) {
+	console.log(i);
+	i++;
+}
+
+var i = 10;
+while (i > 5) {
+	console.log(i);
+	i--;
+}
+
+var i = 0;
+do {
+	console.log(i);
+	i++;
+}while (i < 5); // выполнится хотябы один раз
+
+for (var i = 0; i < 10; i++) {
+	console.log (i);
+}
