3cb66d1cd6448ff9389fa2d4c395ece2976e7521
AnnaIoanovna <ann.palazhchenko@gmail.com>
2016-12-20 19:38:03 +0200

    added home work

---
+++ js/homework.js
@@ -0,0 +1,28 @@
+var x, y, z;
+x = 30;
+y = "car";
+z = 10;
+// console.log(typeof (x) == "string" && y == 10 && z >= y);
+
+// console.log((x > 10 || x > y) && (typeof (y) == "number" || z%2));
+
+// console.log((x+y)%10 == 0 && ((x + y + z) * 2 > 100 ));
+
+// var x = (y%3 == 0) ? y : (z > y) ? z : z + y;
+
+// console.log(x);
+
+// if typeof y =="string" {
+// 	x = y;
+// }
+// else x = z;
+
+console.log(x);
+
+if y == true {
+	z = 5 > 3;
+	x = z;
+}
+else x = false;
+
+console.log(x);
