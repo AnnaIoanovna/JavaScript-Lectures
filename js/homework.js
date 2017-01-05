dda0709c7646c3bc7bef0d575c813e98e88f53ae
AnnaIoanovna <ann.palazhchenko@gmail.com>
2017-01-05 21:58:21 +0200

    array

--- js/homework.js
+++ js/homework.js
@@ -1,25 +1,34 @@
-var x, y, z;
-x = 30;
-y = false;
-z = "4";
+// var x, y, z;
+// x = 30;
+// y = false;
+// z = "4";
+
+// //1
 // console.log(typeof (x) == "string" && y == 10 && z >= y);
 
+// //2
 // console.log((x > 10 || x > y) && (typeof (y) == "number" ||  z % 2 ==0));
 
+// //3
 // console.log((x+y)%10 == 0 && ((x + y + z)/3 * 2 > 100 ));
 
+// //4
 // var x = (y%3 == 0) ? y : (z > y) ? z : z + y;
 
 // console.log(x);
 
-//  if (typeof y =="string" ) {
+// //5
+
+//  if (typeof y !== "number" && y !== null) {
 // 	x = y;
 // }
-//  else if (typeof z =="string" ) {
+//  else if (typeof z !== "number" && z !== null) {
 //  	x = z
 //  }
 
 // console.log(x);
+
+// //6
 
 // if (y !== false) {
 // 	x = "true";
@@ -28,43 +37,46 @@ z = "4";
 
 // console.log(x);
 
-// switch (y) {
-// 	case 0:
-// 	x = y;
-// 	break;
-// 	case false:
-// 	x = y;
-// 	break;
-// 	case undefined:
-// 	x = y;
-// 	break;
-// 	case null:
-// 	x = y;
-// 	break;
-// 	default:
-// 	x = +(z);
-// 	break;
-// }
-// console.log (x);
+// //7
 
 // switch (y) {
 // 	case 0:
 // 	x = y;
-	
+// 	break;
 // 	case false:
 // 	x = y;
-	
+// 	break;
 // 	case undefined:
 // 	x = y;
-	
+// 	break;
 // 	case null:
 // 	x = y;
 // 	break;
 // 	default:
-// 	x = +"z";
+// 	x = +z;
+// 	break;
+// }
+// console.log (x);
+
+// //8
+
+// switch (y) {
+// 	case 0:
+	
+// 	case false:
+		
+// 	case undefined:
+		
+// 	case null:
+// 	x = y;
+// 	break;
+// 	default:
+// 	x = +z;
 	
 // }
 // console.log (x);
+
+// //9
 
 // var arr = ["", "0", 0, true, undefined, NaN, 234, "ololo", -1, [], {}];
 // var newArr = [];
@@ -74,6 +86,8 @@ z = "4";
 // 	}
 // }
 // console.log (newArr);	
+
+// //10
 // var newArr1 = [];
 // var j = 0;
 // while (j < arr.length) {
@@ -89,25 +103,31 @@ z = "4";
 // }else if(){}
 
 
+//11
+
 var baseArray = [];
-var i=1;
-for (i==1; i<100; i++) {
+
+for (var i = 1; i<100; i++) {
 	baseArray.push(i);
 }
 var newArray1 = [];
 var newArray2 = [];
 var newArray3 = [];
 
-for (baseArray[i]==1; [i] < baseArray.length; i+=3){
-	newArray1.push(baseArray[i]);
+var count = 0;
+
+for (var i = 0; i < baseArray.length; i++){
+	count++
+	if (count == 1) {
+	newArray1.push(baseArray[i]);}
+	else if  (count == 2) {
+	newArray2.push(baseArray[i]); }
+	else if  (count == 3) {
+	newArray3.push(baseArray[i]);
+	count = 0}
 }
 
-for (baseArray[i]==2; [i] < baseArray.length; i+=3){
-	newArray2.push(baseArray[i]);
-}
-for (baseArray[i]==3; [i] < baseArray.length; i+=3){
-	newArray3.push(baseArray[i]);
-}
+
 console.log (newArray1);
 console.log (newArray2);
 console.log (newArray3);
--- js/main.js
+++ js/main.js
@@ -70,21 +70,67 @@
 // fnName(1, 2, 3);
 // fnName(5, 4, 9);
 
-var matrix = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9]
-];
+// var matrix = [
+// 	[1, 2, 3],
+// 	[4, 5, 6],
+// 	[7, 8, 9]
+// ];
 
-function findFiveInMatrix(matrix) {
-	outer: for (var i = 0; i < matrix.length; i++) {
-		console.log("outer iteaftion", i, "value = ", matrix[i]);
-		inner: for (var j = 0; j < matrix.length; j++) {
-			console.log("inner iteration =", j, "value =", matrix[i][j]);
-			if (matrix[i][j] == 5) {
-				break outer;
-			}
-		}
+// function findFiveInMatrix(matrix) {
+// 	outer: for (var i = 0; i < matrix.length; i++) {
+// 		console.log("outer iteaftion", i, "value = ", matrix[i]);
+// 		inner: for (var j = 0; j < matrix.length; j++) {
+// 			console.log("inner iteration =", j, "value =", matrix[i][j]);
+// 			if (matrix[i][j] == 5) {
+// 				break outer;
+// 			}
+// 		}
+// 	}
+// };
+// findFiveInMatrix(matrix);
+
+
+// var obj = {
+// 	"key1": "value1",
+// 	"key2": "value2",
+// 	"key3": "value3"
+// }
+
+// var array = ["arrValue1", "arrValue2", "arrValue3"];
+// array.myPronerty = "test";
+// for (var key in obj) {
+// 	console.log(obj[key]);
+// }
+
+// for (var index in array) {
+// 	console.log(array[index]);
+// }
+
+// array.forEach(function(element, index, iterableArray) {
+// 	console.log(element, index, iterableArray);
+// });
+
+// var array = [];
+
+// array[10] = null;
+
+// console.log(array);
+
+// console.log(array.length);
+
+// var array = [];
+
+// var object = {};
+
+// console.log(typeof array);
+// console.log(typeof object);
+// console.log(Array.isArray(array));
+// console.log(Array.isArray(object));
+var arr = ["", "0", 0, true, undefined, NaN, 234, "ololo", -1, [], {}];
+var newArr = [];
+for (var i = 0; i < arr.length; i++) {
+	if (arr[i] == false ) {
+		newArr.push(arr[i]);
 	}
-};
-findFiveInMatrix(matrix);
+}
+console.log (newArr);	
