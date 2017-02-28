d37bc541db05c25c6357d7a8abcc30cd59afda7b
AnnaIoanovna <ann.palazhchenko@gmail.com>
2017-02-28 22:10:41 +0200

    Method

---
+++ js/homework.js
@@ -0,0 +1,199 @@
+ function getRandomInt(min, max) {
+ return Math.floor(Math.random()*(max - min + 1)) + min;
+}
+
+// function addOpacity(hexColor, opasity) {
+// 	 var r = parseInt(hexColor.slice(1, 3), 16),
+//          g = parseInt(hexColor.slice(3, 5), 16),
+//         b = parseInt(hexColor.slice(5, 7), 16);
+//          return "rgba(" + r + ", " + g + ", " + b + ", " + opasity + ")";
+// }
+// console.log(addOpacity("#324685", 0.8));
+
+// function blockGrower(width, height) {
+// 	widthInt = parseFloat(width);
+// 	heightInt = parseFloat(height);
+// 	widthStr = width.slice(widthInt.toString().length);
+// 	heightStr = height.slice(heightInt.toString().length);
+// 	widthInt = Math.round(widthInt * 1.5 * 10000)/10000;
+// 	heightInt = Math.round(heightInt * 1.5 * 10000)/10000;
+	
+// 	width = widthInt + widthStr;
+// 	height = heightInt + heightStr
+// 	return {width, height};
+// }
+// console.log(blockGrower("3.444%", "5em"));
+
+// function getTwoNumders(min, max) {
+// 	var values = [];
+// 	for (var i = 0; i < 5; i++) {
+
+// 		values.push(getRandomInt(min, max));
+// 	}
+// 	console.log(values);
+// 	min = values[0];
+//   	max = min;
+//   for (i = 1; i < values.length; ++i) {
+//       if (values[i] > max) max = values[i];
+//       if (values[i] < min) min = values[i];
+//   }
+//   return {min, max};
+// }
+// console.log(getTwoNumders(44, 99));
+
+// function checkNumberType(number) {
+// 	if (parseInt(number) != number) {
+// 		return ("float");}
+// 	else {
+// 		return ("integer");
+// 	}	
+// }
+//  console.log(checkNumberType(99.7));
+
+// function getToRandomPart(string) {
+// 	var intFirst = getRandomInt(0, string.length);
+// 	var intSec = getRandomInt(intFirst, string.length);
+// 	var strPart = string.slice(intFirst, intSec);
+// 	console.log(intFirst);
+// 	console.log(intSec);
+// 	return strPart;
+// }
+
+// console.log(getToRandomPart("ghjdslrofdnk"));
+
+// function getTwoSymbols(string) {
+// 	var firstInd = getRandomInt(0, string.length-1);
+// 	var secInd = getRandomInt(0, string.length-1);
+// 	if (secInd == firstInd) {
+// 		getRandomInt(0, string.length-1);
+// 	}
+// 	var sumbolFirst = string.charAt(firstInd); 
+// 	var sumbolSecond = string.charAt(secInd);
+// 	if (sumbolFirst != sumbolSecond) {
+// 		return {sumbolFirst: sumbolFirst, sumbolSecond: sumbolSecond};
+// 	}
+// 	else return (sumbolFirst);
+// }
+// console.log(getTwoSymbols("fgiuioFSOISk"));
+
+// function wordRemover(word, string) {
+// 	var wd = new RegExp(word, "g");
+// 	string = string.replace(wd, "");
+// 	return string;
+// }
+// console.log(wordRemover("cat", "fsdcatoljfcatd"));
+
+// function getObjectsValues(string) {
+// 	var res = [];
+// 	for (key in string) {
+// 		if (typeof string[key] == "object") {
+// 			res = res.concat(getObjectsValues(string[key]));
+
+// 		}
+// 		else {
+// 		res.push(string[key]);
+// 		}
+// 	}
+// 	return res;
+// }
+// console.log(getObjectsValues({name:"Jon", age:25, animals:{type:"cat", name:"Blohastik", toy:{type:"boll", size: "small"}}}));
+
+// function getRequiredSymbols(string, type) {
+// 	var result = "";
+// 	var arr = string.split("");
+// 	var arrtype = [];
+// 	for (var i = 0; i < arr.length; i++) {
+
+// 		if (!isNaN (parseInt(arr[i])) && type == "number")  {
+// 			arrtype.push(arr[i]);
+// 		}
+// 		if (isNaN(parseInt(arr[i])) && type != "number") {
+// 			arrtype.push(arr[i]);
+// 		}
+// 	}
+// 		result = arrtype.join("");
+	
+// 	return result;	
+// }
+// console.log(getRequiredSymbols("ajjjdh57kjj764dkfl666", "number"));
+// console.log(getRequiredSymbols("ajjjdh57kjj764dkfl666", "string"));
+
+// function getSostedString(tree) {
+// 	var arr = tree.split("");
+// 	var arrint = [];
+// 	var arrstr = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (!isNaN (parseInt(arr[i]))) {
+// 			arrint.push(arr[i]);
+// 		}
+// 		if (isNaN (parseInt(arr[i]))) {
+// 			arrstr.push(arr[i]);
+// 		}
+// 	};
+// 	arrint = arrint.sort(function(a,b){
+// 		if (a > b)
+// 			return 1;
+// 		if (a < b)
+// 			return -1;
+// 	});
+// 	arrstr = arrstr.sort(function anyCase(a, b) {
+// 		if (a.toLowerCase() > b.toLowerCase())
+//  		return 1;
+// 		if (a.toLowerCase() < b.toLowerCase())
+//   		return -1;
+// 		else
+//   		return 0;
+// 	});
+// 	var newArr = arrint.concat(arrstr);
+// 	return newArr.join("");
+// }
+// console.log(getSostedString("fhus66544albiof"));
+
+// function getIntervedNumbers(arrayOfNumber) {
+// 	var newArrayOfNumber = [];
+// 	for (var i = 0; i < arrayOfNumber.length; i++) {
+// 		newArrayOfNumber.push(-arrayOfNumber[i]);
+// 	}
+// 	return newArrayOfNumber;
+// }
+// console.log(getIntervedNumbers([-3, -5, -4, 6, 7, 9]));
+
+// function getReversedString(string){
+// 		var arr = string.split("");
+// 		arr.reverse();
+// 		return arr.join("");
+// }
+
+// console.log(getReversedString("qwerty"));
+
+// function namesConcatenator(array) {
+//  return array.map(function (elem) {
+//   return {
+//    fullName: elem.firstName + " " +elem.lastName,
+//    age: elem.age,
+//    gender: elem.gender
+//   }
+//  });
+// }
+
+// console.log (namesConcatenator({firstName: "Jon", lastName:"Smitt", age: 25, gender:"man"}))
+
+// function getBorderNumbers(arrayOfNumber, takeForm) {
+// 	if (takeForm == "first") {
+// 		return arrayOfNumber.slice(0, 2);
+// 	}
+// 	if (takeForm == "last") {
+// 		return arrayOfNumber.slice(-2);
+// 	}
+// }
+// console.log(getBorderNumbers([6,4,33,74,2,5,7], "last"));
+
+function getTotalPrice(arrayOfProducts) {
+	arrayOfProducts.map(function(elem){
+		if (elem.inCart == true) 
+		var summ = summ + parseFloat(n$); 
+	})
+	return summ;
+}
+arrayOfProducts = {name: "dog", price: 5$, inCart:true};
+console.log(getTotalPrice(arrayOfProducts));
--- js/main.js
+++ js/main.js
@@ -1,21 +1,183 @@
-function test(arg1, arg2) {
-	test2.apply(this, arguments);
-	return this;
-}
-function test2(arg1, arg2) {
-	console.log(this, arg1, arg2);
-}
-var obj = {
-	name: "slovo",
-	test: test
-}
-var obj2 = {ololo: "trololo"};
+// function test(arg1, arg2) {
+// 	test2.apply(this, arguments);
+// 	return this;
+// }
+// function test2(arg1, arg2) {
+// 	console.log(this, arg1, arg2);
+// }
+// var obj = {
+// 	name: "slovo",
+// 	test: test
+// }
+// var obj2 = {ololo: "trololo"};
 //console.log(obj.test());
 //console.log(test());
 
 //console.log(test.call(obj2, "arg1", "arg2"));
 //console.log(test.apply(obj2, ["arg1", "arg2"]));
-test.apply(obj2, ["argument1","argument2"]);
+// test.apply(obj2, ["argument1","argument2"]);
 
-var bindedTest2 = test2.bind(obj2);
-bindedTest2("sad", "bad");
+// var bindedTest2 = test2.bind(obj2);
+// bindedTest2("sad", "bad");
+
+// function bindLate(context, funcName) {
+// 	return function() {
+// 		return context[funcName].apply(context, arguments);
+// 	}	
+// }
+// var obj = {
+// 	method: function(number) {console.log("Old Method" + number )}
+// }
+// var lateBind = bindLate(obj, "method");
+// lateBind(1);
+// obj.method = function() {console.log("New Method")}
+// lateBind(2);
+
+
+// function callback(e) {
+// 	console.log(this);
+// 	console.log(e.target);
+// 	console.log(e);
+// }
+// callback({});
+// var body = document.querySelector("body");
+// //body.addEventListener("click", callback);
+
+// //$("a").css("display", "none").hide().fadeIn(500);
+
+// var object = {
+// 	counter: 0,
+// 	increment: function() {
+// 		this.counter++;
+// console.log(this.counter);
+// return this;
+// 	},
+// 	decrement: function() {
+// 		this.counter--;
+// console.log(this.counter);
+// return this;
+// 	},
+// callbackCaller: function(callbackForCall) {
+// 	callbackForCall.call(this);
+// 	callbackForCall();
+// 	return this;
+// 	}	
+// }
+// function myCallback() {
+// 	console.log(this);
+//}
+
+// object.increment();
+// object.decrement();
+// object.increment();
+// object.increment();
+
+// object
+// 	.increment()
+// 	.decrement()
+// 	.increment()
+// 	.increment()
+// 	.callbackCaller(myCallback);
+
+// function timeout() {
+// 	var x = 5;
+// 	console.log(x);
+// 	x++;
+// 	setTimeout(function(){
+// 		console.log(x)
+// 	}, 1000);
+// }
+// timeout();
+// timeout();
+
+// function event() {
+// 	var x = 0;
+// 	console.log(x);
+// 	body.addEventListener("click", function() {
+// 		x++;
+// 		console.log(x);
+// 	})
+// }
+// event();
+
+// function closure() {
+// 	var x = "a";
+// 	console.log(x);
+// 	return function(val) {
+// 		x+= val;
+// 		console.log(x);
+// 	}
+// }
+// // var closuredFunction = closure ();
+// // closuredFunction(1);
+// // closuredFunction(null);
+// // closuredFunction("oloshenki");
+
+// function closure2() {
+// 	var array = [];
+// 	return  {
+// 		addElem: function(elem) {
+// 			array.push(elem);
+// 			return this;
+// 		},
+// 		removeElem: function(index) {
+// 			array.splice(index, 1);
+// 			return this;
+// 		},
+// 		showStorage: function() {
+// 			console.log(array);
+// 			return this;
+// 		}
+// 	}
+// }
+
+// var storage = closure2();
+// storage
+// 	.addElem("text")
+// 	.addElem(2)
+// 	.showStorage()
+// 	.removeElem(1)
+// 	.showStorage();
+
+// var storage = closure2();
+// storage
+// 	.addElem(1)
+// 	.addElem(5)
+// 	.showStorage()
+// 	.removeElem(1)
+// 	.showStorage();
+// function getType(elem) {
+// 	return {}.toString.call(elem);
+// }
+
+// console.log
+
+(function(){
+	var myAplication = {};
+	myAplication.method1 = method1;
+	myAplication.method2 = method2;	
+	myAplication.method2 = method3;
+	myAplication.addMethod = addMethod;
+
+	window.api = myAplication;
+
+	function method1 () {
+		console.log("Method 1");
+	}
+
+	function method2 () {
+		console.log("Method 2");
+	}
+	function method3 () {
+		console.log("Method 3");
+	}
+	function addMethod(name, func) {
+		if (!myAplication[name]) {
+			myAplication[name] = func;
+		}
+		else {
+			throw("This method alrady exist");
+		}
+	}
+
+})();
