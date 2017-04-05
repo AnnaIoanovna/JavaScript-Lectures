cde2977a43796ee5267e218c2c780faf0c3fb125
AnnaIoanovna <ann.palazhchenko@gmail.com>
2017-04-05 21:02:14 +0300

    Jquery

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//console.log($("h1").parent());
+//console.log($("h1").parents());
+//console.log($("h1").children());
+//console.log($("h1").children("img"));
+//console.log($("body").find("img"));
+//console.log($("h1 img").next());
+//console.log($("h1 span").prev());
+
+// $("h1").on("click", function(){
+// 	$(this).addClass("active");
+// })
+
+// $("h1").on("click", function(){
+// 	$(this).removeClass("mainTitle");
+// })
+// $("h1").on("click", function(){
+// 	$(this).toggleClass("mainTitle");
+// })
+
+//console.log($("h1").hasClass("mainTitle"));
+// console.log($("h1").attr("class"));
+// $("h1").removeAttr("class");
+// console.log($("h1").prop("class"));
+// console.log($(":text").val());
+// $(":text").val("new text");
+// console.log($(":text").val());
+
+// console.log($(".mainTitle").html());
+// $(".mainTitle").html("<h1>Ololollo io</h1>");
+
+// var progress = $("progress")
+// setInterval (function(){
+// 	$(progress).val($(progress).val() + 0.01);
+// 	if ($(progress).val()>=1) {
+// 		$(progress).val(0);
+// 	}
+// }, 100);
